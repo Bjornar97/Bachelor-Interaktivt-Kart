9565714afef9be1ef740e8a4ded8f5d7816481ee
Bjornar97 <hetlest@hotmail.com>
2019-02-07 11:03:58 +0100

    Lagde location service
    - Lagde location service med funksjoner for å få posisjonen til telefonen.
    - La til en foreløpig knapp for å fly til din posisjon
    - Gjorde hoved-kartet til en global variabel i globals.ts
    - Bindet kartvalg i map.component.html til variabler i koden.

--- KartApp/src/app/map/map.component.ts
+++ KartApp/src/app/map/map.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { MapboxViewApi, Viewport as MapboxViewport } from "nativescript-mapbox";
+import { LocationService } from "../location.service";
+import { MainMap, setMap, buttons } from "../globals";
 
 @Component({
   selector: 'ns-map',
@@ -8,42 +10,103 @@ import { MapboxViewApi, Viewport as MapboxViewport } from "nativescript-mapbox";
   moduleId: module.id,
 })
 export class MapComponent implements OnInit {
+    
+    constructor(){
+        this.locationService = new LocationService(1);
+    }
 
-  private map: MapboxViewApi;
+    @Input() main: string;
+    private locationService: LocationService;
+    private map: MapboxViewApi;
+    private mapStyles = {
+        mapStyle: "mapbox://styles/mapbox/outdoors-v10",
+        latitude: 64.810928,
+        longitude: 15.605580,
+        zoomLevel: 3,
+        showUser: true,
+        disableMovement: false
+    }
+
+    /**
+     * flyTo(): Flies the camera to the specified location or the current location of the device.
+     * 
+     * zoomLevel: How far the camera should zoom in(only applies to android). Default: 14.
+     * 
+     * altitude: How high in meters the camera should stop above ground(only applies to iOS)
+     * 
+     * bearing: The direction of the compass in degrees. If not specified, it is the same as it was.
+     * 
+     * duration: For how long the animation should take in milliseconds. Default: 4000
+     * 
+     * maxAge: How old the location reading can be in milliseconds.
+     * 
+     * LocationObject: An object that need latitude and longitude which is numbers if specified.
+     * 
+     */
+    flyTo(zoomLevel = 14, altitude = 5000, bearing?, duration = 4000, maxAge?, LocationObject?) {
+        // Function to get the camera to fly to the current location of the device
+        var map = this.map;
+        var location;
+        console.log("map.component: Getting ready to fly");
+        if (LocationObject == undefined){
+            console.log("map.component: location not specified. Trying to find current location");
+            this.locationService.getLocation(maxAge, 10000, 0)
+                .then(function(result){
+                    location = result;
+                    map.animateCamera({
+                        // Target is the destination for the camera, which is the location of the device
+                        target: {
+                            lat: location.latitude, 
+                            lng: location.longitude
+                        },
+                        zoomLevel: zoomLevel, // Zoom level on Android
+                        altitude: altitude, // altitude above the ground in metres on iOS
+                        bearing: bearing, // The direction of the camera in degrees(0 - 360)
+                        duration: duration // How long the animation lasts
+                    });
+                }, function(err){
+                    console.log("map.component: An error occured. " + err);
+                    return new Error("Could not get location" + err);
+                });
+        } else {
+            console.log("map.component: Got a locationObject");
+            location = LocationObject;
+            map.animateCamera({
+                // Target is the destination for the camera, which is the location of the device
+                target: {
+                    lat: location.latitude, 
+                    lng: location.longitude
+                },
+                zoomLevel: 14, // Zoom level on Android
+                altitude: 5000, // altitude above the ground in metres on iOS
+                bearing: 0, // The direction of the camera in degrees(0 - 360)
+                duration: 4000 // How long the animation lasts
+            });
+        }
+    }
 
   // Waits until the map is ready
   onMapReady(args): void {
       // Gets the map that is displayed
       this.map = args.map;
-      
-      // Waits 2 seconds
-      setTimeout(() => {
-          // Put this into a local variable, because this cannot be called in the function below
-          var _this = this;
+      if (this.main == "true"){
+        setMap(this);
+      }
 
-          // Function in the plugin for getting the location of the device and put it into the variable userLocation
-          this.map.getUserLocation().then(
-              function(userLocation) {
-                  // Function to get the camera to fly to the current location of the device
-                  _this.map.animateCamera({
-                      // Target is the destination for the camera, which is the location of the device
-                      target: {
-                          lat: userLocation.location.lat, 
-                          lng: userLocation.location.lng
-                      },
-                      zoomLevel: 14, // Zoom level on Android
-                      altitude: 5000, // altitude above the ground in metres on iOS
-                      bearing: 0, // The direction of the camera in degrees(0 - 360)
-                      duration: 4000 // How long the animation lasts
-                  });
-              }
-          )
-      }, 2000);
+      console.log("Trying to find location");
+      var map = this.map;
+      this.locationService.getLocation(undefined, 3000, 0).then(function (loc) {
+          console.log("Latitude: " + loc.latitude + ", longitude: " + loc.longitude);
+          map.setCenter({lat: loc.latitude, lng: loc.longitude, animated: false});
+          map.setZoomLevel({level: 16, animated: false});
+      }, function (err) {
+          console.log("ERROR: ");
+          console.dir(err);
+      });
 
   }
 
   ngOnInit() {
-    console.log("initted map in component")
   }
 
 }
