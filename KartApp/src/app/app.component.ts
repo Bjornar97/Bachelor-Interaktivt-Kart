e8c7966565f2ae035c0d5a8e93452f7c1a8ea6ad
mnkvm <35263582+mnkvm@users.noreply.github.com>
2019-02-18 10:22:42 +0100

    Laget setDrawerHeight() funksjon

--- KartApp/src/app/app.component.ts
+++ KartApp/src/app/app.component.ts
@@ -19,14 +19,14 @@ export class AppComponent {
     private showLocationButton = true;
 
     private drawer = {
-        startHeight: 100,
+        startHeight: 100, // Brukt til å renge ut høyden ved endring i høyde.
         heightInt: 100,
         height: "100dp",
         visibility: "visibility: collapsed;",
         drawerClass: "drawer",
         maxHeight: screen.mainScreen.heightDIPs - 113,
         maxHeightLocationButton: screen.mainScreen.heightDIPs / 2,
-        initialHeight: 100
+        initialHeight: 200 // Høyden den husker og starter på når du åpner draweren.
     };
 
     private buttons = {
@@ -49,8 +49,48 @@ export class AppComponent {
         this.drawer.visibility = "visibility: visible;";
     }
 
-    setDrawerHeight(height = 0){
+    closeDrawer(){
+        this.hideDrawer()
+        this.setSelectedButtons()
+        this.showLocationButton = true;
+    }
+
+    setDrawerHeight(height = 0, isPanning = false){
+        var drawerLoc = this.drawer;
+
+        if (height < 0){
+            console.log("Drawer height below minimum range! Height: " + height + ", Minimum: 0")
+            height = 0;
+        }
+        if (height > drawerLoc.maxHeight){
+            console.log("Drawer height above maximum range! Height: " + height + ", Maximum: " + drawerLoc.maxHeight)
+            height = drawerLoc.maxHeight;
+        }
+
+        if (isPanning) {
+            
+        } else {
+            // Når draweren er på toppen
+            if (height > drawerLoc.maxHeight - 20){
+                height = drawerLoc.maxHeight;
+                drawerLoc.drawerClass = "drawerMaximized";
+            }
+            // Gjem draweren når den er dratt helt ned
+            if (height < 10){
+                this.closeDrawer();
+            } else {
+                drawerLoc.initialHeight = height;
+            }
+        }
         
+        if (height >= drawerLoc.maxHeightLocationButton){
+            this.showLocationButton = false;
+        } else {
+            this.showLocationButton = true;
+        }
+
+        drawerLoc.heightInt = height;
+        drawerLoc.height = height + "dp";
     }
 
     setSelectedButtons(selectedButton = null){
@@ -90,6 +130,7 @@ export class AppComponent {
         if (state === 1) {
             // Første trykk
             drawerLoc.drawerClass = "drawer";
+            drawerLoc.startHeight = drawerLoc.heightInt;
         }
 
         if (state === 2) {
@@ -120,9 +161,8 @@ export class AppComponent {
                 this.hideDrawer();
                 this.setSelectedButtons();
             } else {
-                this.drawer.initialHeight = this.drawer.heightInt;
+                drawerLoc.initialHeight = drawerLoc.heightInt;
             }
-            drawerLoc.startHeight = drawerLoc.heightInt;
         }
         
         if (drawerLoc.heightInt >= drawerLoc.maxHeightLocationButton){
@@ -140,7 +180,7 @@ export class AppComponent {
         console.log(buttonName)
         if (this.isSelected(buttonName)){
             buttonName = null;
-            this.hideDrawer();
+            this.closeDrawer();
         } else {
             this.showDrawer();
         }
@@ -161,7 +201,6 @@ export class AppComponent {
         }
 
         this.drawer.height = this.drawer.heightInt + "dp";
-        this.drawer.startHeight = this.drawer.heightInt;
 
 
         this.drawer.initialHeight = this.drawer.heightInt;
