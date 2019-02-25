09256cb77618446adc786efc7fad105cdf1f307a
Bjornar97 <hetlest@hotmail.com>
2019-02-25 22:51:22 +0100

    Laget ny tur siden og laget tripService
    - Laget en side for nå en tur pågår
    - Laget en service som skal brukes til å få informasjon om turer, og adminsistrere turer.
    - Lagt til en tilbakeknapp i dra-opp-tingen
    - Implementert mulighet for å sette en tur på pause, og fortsette den.
    - Implementert lagring av turer
    - Implementert fortsetting av tur som ikke ble ferdig.
    - Begynt på en side for visning av tur informasjon
    - Implementert mulighet for å ta bilder mens du går på tur(NB: Blir ikke lagret enda)
    - Laget en knapp for hjem-siden som lager en ny tur  eller går til en pågående tur
    
    ## I Trip-typen:
    - Lagt til watchID, som brukes til å avslutte geotracking
    - lagt til pauses, som ineholder en liste over pauser i turen
    
    + andre småting

---
+++ KartApp/src/app/home-page/trip-box/trip-box.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ns-trip-box',
+  templateUrl: './trip-box.component.html',
+  styleUrls: ['./trip-box.component.css'],
+  moduleId: module.id,
+})
+export class TripBoxComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
