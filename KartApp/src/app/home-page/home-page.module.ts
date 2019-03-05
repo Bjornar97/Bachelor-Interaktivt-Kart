c547532ce257be249f6a0ed84df541a3e2d0dbe1
Bjornar97 <hetlest@hotmail.com>
2019-03-05 23:16:11 +0100

    La til skygger og endret farge på tur-strekene til rød
    - La til ny skygge plugin
    - La til skygge på trip-box
    - La til en slags border på gå til posisjon knappen og endret farge til en lys blå farge når du trykker på den.

--- KartApp/src/app/home-page/home-page.module.ts
+++ KartApp/src/app/home-page/home-page.module.ts
@@ -6,11 +6,14 @@ import { HomePageComponent } from "./home-page.component";
 import { TripBoxComponent } from './trip-box/trip-box.component';
 import { TripPageComponent } from './trip-page/trip-page.component';
 import { CurrentTripPageComponent } from './current-trip-page/current-trip-page.component';
+import { NgShadowModule } from 'nativescript-ng-shadow';
+
 
 @NgModule({
     imports: [
         NativeScriptCommonModule,
-        HomeRoutingModule
+        HomeRoutingModule,
+        NgShadowModule
     ],
     declarations: [
         HomePageComponent,
