855b40259a036db583533e2a5929087e685f3c1d
Bjornar97 <hetlest@hotmail.com>
2019-03-17 23:52:04 +0100

    Lagde funksjon for å få events fra en tur
    - Funksjon som kalkulerer events og returnerer dem
    - Endret pauser fra å ha timestamps til å ha posisjoner.
    - Fikset en bug hvor søndag kom frem som undefined.
    - La til duration og distanceMeters i trip. Slik at totale tid og distanse ikke trengs å kalkuleres hver gang.

--- KartApp/src/app/home-page/trip-page/trip-page.component.ts
+++ KartApp/src/app/home-page/trip-page/trip-page.component.ts
@@ -6,6 +6,7 @@ import { MainMap } from '~/app/globals';
 import { Page, booleanConverter } from 'tns-core-modules/ui/page/page';
 import { ActivatedRoute } from '@angular/router';
 import { isAndroid } from "platform";
+import { Trip } from '~/app/tracker';
 
 @Component({
   selector: 'ns-trip-page',
@@ -22,7 +23,7 @@ export class TripPageComponent implements OnInit, OnDestroy {
 
   private sub;
   private backEvent;
-  private trip;
+  private trip: Trip;
 
   ngOnInit() {
     this.sub = this.route.params.subscribe(params => {
@@ -49,6 +50,8 @@ export class TripPageComponent implements OnInit, OnDestroy {
     });
     MainMap.removeLine();
     MainMap.drawLine(this.trip.points, "green", 4, 1);
+
+    console.log(this.tripService.getTripEvents(this.trip.id));
   }
 
   ngOnDestroy(){
