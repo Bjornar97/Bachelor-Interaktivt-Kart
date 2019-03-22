deeb47ab084046dbdc503b10a98e69368d3ddf46
Bjornar97 <hetlest@hotmail.com>
2019-03-22 10:42:39 +0100

    Fikset en til duplikat

--- KartApp/src/app/home-page/trip-page/trip-page.component.ts
+++ KartApp/src/app/home-page/trip-page/trip-page.component.ts
@@ -13,7 +13,7 @@ import { ImageService } from '../image.service';
 
 let days = ["Mandag", "Tirsdag", "Onsdag", "Torsdag", "Fredag", "Lørdag", "Søndag"];
 
-let days = ["Mandag", "Tirsdag", "Onsdag", "Torsdag", "Fredag", "Lørdag", "Søndag"];
+let days = ["Søndag", "Mandag", "Tirsdag", "Onsdag", "Torsdag", "Fredag", "Lørdag"];
 
 @Component({
   selector: 'ns-trip-page',
