67c2578072a3c3541841dd44a70785d916cfe671
Naomi <naomypop@yahoo.com>
2019-03-29 13:08:30 +0100

    Fikset ting

--- KartApp/src/app/home-page/trip.service.ts
+++ KartApp/src/app/home-page/trip.service.ts
@@ -162,10 +162,10 @@ export class TripService {
   sortTrips(trips: Trip[]){
     // Sortere etter startTime. f. eks: trip[x].startTime
     return trips.sort((n1,n2) => {
-    if (n1.startTime.getTime() > n2.startTime.getTime()) {
+    if (n1.startTime > n2.startTime) {
       return -1;
   }
-    if (n1.startTime.getTime() < n2.startTime.getTime()) {
+    if (n1.startTime < n2.startTime) {
       return 1;
   }
       return 0;
