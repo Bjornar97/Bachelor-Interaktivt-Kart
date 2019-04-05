92a8888002e85d2cd34d0ef5d32077850040cf46
Bjornar97 <hetlest@hotmail.com>
2019-04-05 11:53:22 +0200

    La til backend-service og metode for login i account-page

--- KartApp/src/app/account-page/account-page.component.ts
+++ KartApp/src/app/account-page/account-page.component.ts
@@ -3,22 +3,35 @@ import { Page } from 'tns-core-modules/ui/page/page';
 import { DrawerClass } from '~/app/drawer';
 import * as globals from "../globals";
 import { Color } from "tns-core-modules/color"; 
+import { BackendService } from './backend.service';
+import { SettingsService } from '../settings-page/settings.service';
 
 @Component({
   selector: 'ns-account-page',
   templateUrl: './account-page.component.html',
   styleUrls: ['./account-page.component.css'],
   moduleId: module.id,
+  providers: [BackendService]
 })
 export class AccountPageComponent implements OnInit {
   
   private drawer: DrawerClass;
 
-  constructor(private page: Page) {
+  constructor(private page: Page, private backendService: BackendService, private settingsService: SettingsService) {
     // Use the component constructor to inject providers.
     this.drawer = globals.getDrawer();
     page.actionBarHidden = true;
-}
+  }
+
+  login(loginName, password){
+    this.backendService.login(loginName, password)
+    .subscribe((result) => {
+      let tokenSetting = this.settingsService.getSetting(undefined, 61);
+      tokenSetting.value = (<any>result).json.data.access_token;
+      this.settingsService.setSetting(tokenSetting);
+    });
+  }
+
 
   ngOnInit() {
     var color=new Color("#0f0");
@@ -30,6 +43,7 @@ export class AccountPageComponent implements OnInit {
       //var placeholder=textField.ios.valueForKey("placeholderLabel")
       //placeholder.textColor=color.ios;
     }
+
   }
 
 }
