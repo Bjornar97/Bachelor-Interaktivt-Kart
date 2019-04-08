2a111b8d79d0d4b4de7b74db22b0cc8d2e288943
mnkvm <35263582+mnkvm@users.noreply.github.com>
2019-04-08 12:45:42 +0200

    Revert "Konto side"

--- KartApp/src/app/account-page/account-page.module.ts
+++ KartApp/src/app/account-page/account-page.module.ts
@@ -1,7 +1,5 @@
 import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
 import { NativeScriptCommonModule } from 'nativescript-angular/common';
-import { NativeScriptHttpClientModule } from "nativescript-angular/http-client";
-
 import { AccountPageComponent } from "./account-page.component";
 import { AccountPageRoutingModule } from "./account-page-routing.module";
 
@@ -11,8 +9,7 @@ import { AccountPageRoutingModule } from "./account-page-routing.module";
   ],
   imports: [
     NativeScriptCommonModule,
-    AccountPageRoutingModule,
-    NativeScriptHttpClientModule
+    AccountPageRoutingModule
   ],
   schemas: [NO_ERRORS_SCHEMA]
 })
