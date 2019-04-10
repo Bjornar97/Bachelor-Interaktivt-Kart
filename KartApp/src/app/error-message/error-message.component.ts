9f53b60297aeda7974ef38c475638e6dec4045d7
mnkvm <35263582+mnkvm@users.noreply.github.com>
2019-04-10 16:35:30 +0200

    Laget error-message component

---
+++ KartApp/src/app/error-message/error-message.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import * as globals from "../globals";
+import { View } from 'tns-core-modules/ui/page/page';
+import * as dialogs from "tns-core-modules/ui/dialogs";
+
+@Component({
+  selector: 'ns-error-message',
+  templateUrl: './error-message.component.html',
+  styleUrls: ['./error-message.component.css'],
+  moduleId: module.id,
+})
+export class ErrorMessageComponent implements OnInit {
+
+  constructor() {
+    
+  }
+
+  private errorList: string[];
+  
+  errorCreatedAnimation(box: View){
+    box.scaleX = 0;
+    box.scaleY = 0;
+    box.animate({
+      scale: {x: 1, y: 1},
+      duration: 200,
+      delay: 0
+    });
+  }
+
+  ngOnInit() {
+    this.errorList = globals.getErrorList();
+  }
+
+}
