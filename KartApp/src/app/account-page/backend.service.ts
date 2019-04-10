13b29d4dbf2d2764b481bb17c5088f9012752690
Bjornar97 <hetlest@hotmail.com>
2019-04-10 15:10:12 +0200

    Fjernet toast og endret token til sånn som den skal være

--- KartApp/src/app/account-page/backend.service.ts
+++ KartApp/src/app/account-page/backend.service.ts
@@ -1,8 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders, HttpParams } from "@angular/common/http";
 import { SettingsService } from '../settings-page/settings.service';
-import * as Toast from 'nativescript-toast';
-
 
 @Injectable({
   providedIn: 'root'
