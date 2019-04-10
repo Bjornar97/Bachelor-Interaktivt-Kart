bc7fa568064533f4e49a30811a056e55df9e9eb2
Bjornar97 <hetlest@hotmail.com>
2019-04-10 17:13:44 +0200

    Endret på layout på account-page

--- KartApp/src/app/account-page/account-page.component.ts
+++ KartApp/src/app/account-page/account-page.component.ts
@@ -1,15 +1,111 @@
 import { Component, OnInit } from '@angular/core';
+import { Page } from 'tns-core-modules/ui/page/page';
+import { DrawerClass } from '~/app/drawer';
 import { BackendService } from './backend.service';
-
+import { SettingsService } from '../settings-page/settings.service';
+import * as globals from "../globals";
+import { RouterExtensions } from 'nativescript-angular/router/router-extensions';
 
 
 @Component({
-    selector: 'ns-account-page',
-    templateUrl: './account-page.component.html',
-    styleUrls: ['./account-page.component.css'],
     moduleId: module.id,
+    selector: "ns-account-page",
+    templateUrl: "account-page.component.html",
+    styleUrls: ['./account-page.component.css'],
     providers: [BackendService]
-  }) 
+})
+ 
 export class AccountPageComponent {
+
+    private drawer: DrawerClass;
+    private username: string;
+
+
+    constructor(private page: Page, private backendService: BackendService, private settingsService: SettingsService, private routerExtensions: RouterExtensions) {
+        // Use the component constructor to inject providers.
+        this.drawer = globals.getDrawer();
+        page.actionBarHidden = true; 
+
+        let token = this.settingsService.getSetting(undefined, 61);
+        if (token == undefined) {
+            console.log("TokenSetting is undefined");
+            this.routerExtensions.navigate(["account", "login"], {
+                animated: true,
+                clearHistory: true,
+                transition: {
+                    name: "slideRight"
+                }
+            });
+        } else if (token.value == undefined){
+            console.log("Token is undefined");
+            this.routerExtensions.navigate(["account", "login"], {
+                animated: true,
+                clearHistory: true,
+                transition: {
+                    name: "slideRight"
+                }
+            });
+        } else {
+            console.log("Token is defined");
+            try {
+                this.backendService.getInfo()
+                .subscribe((result) => {
+                    console.dir(<any>result);
+                    if(<any>result.status == 202){
+                        this.userInfo = (<any>result).body.username
+                    } else {
+                        this.routerExtensions.navigate(["account", "login"], {
+                            animated: true,
+                            clearHistory: true,
+                            transition: {
+                                name: "slideRight"
+                            }
+                        });
+                    }
+                });
+            } catch (error) {
+                console.log("Error while getting username: " + error);
+                this.routerExtensions.navigate(["account", "login"], {
+                    animated: true,
+                    clearHistory: true,
+                    transition: {
+                        name: "slideRight"
+                    }
+                });
+            }
+            
+        }
+    }
+
+    private userInfo;
+
+    logOut(){
+        let tokenSetting = this.settingsService.getSetting(undefined, 61);
+        this.backendService.logOut()
+        .subscribe((result) => {
+            console.dir(result);
+            if (<any>result.status == 200 || <any>result.status == 401){
+                if (tokenSetting == undefined){
+                    tokenSetting = {
+                    id: 61,
+                    name: "tokenSetting",
+                    type: "token",
+                    value: undefined
+                    }
+                    tokenSetting.value=undefined;
+                    this.settingsService.setSetting(tokenSetting);
+                }
+                this.routerExtensions.navigate(["account", "login"], {
+                    animated: true,
+                    clearHistory: true,
+                    transition: {
+                        name: "slideRight"
+                    }
+                });
+            } else {
+
+            }
+        });
+    }
 
 }
--- KartApp/src/app/account-page/login-page/login-page.component.ts
+++ KartApp/src/app/account-page/login-page/login-page.component.ts
@@ -1,111 +1,64 @@
 import { Component, OnInit } from '@angular/core';
 import { Page } from 'tns-core-modules/ui/page/page';
 import { DrawerClass } from '~/app/drawer';
-import { BackendService } from './backend.service';
-import { SettingsService } from '../settings-page/settings.service';
-import * as globals from "../globals";
+import * as globals from "../../globals";
+import { Color } from "tns-core-modules/color"; 
+import { BackendService } from '../backend.service';
+import { SettingsService } from '../../settings-page/settings.service';
+import { NativeScriptRouterModule } from "nativescript-angular/router";
 import { RouterExtensions } from 'nativescript-angular/router/router-extensions';
 
-
 @Component({
-    moduleId: module.id,
-    selector: "ns-account-page",
-    templateUrl: "account-page.component.html",
-    styleUrls: ['./account-page.component.css'],
-    providers: [BackendService]
-})
- 
-export class AccountPageComponent {
+  selector: 'ns-login-page',
+  templateUrl: './login-page.component.html',
+  styleUrls: ['./login-page.component.css'],
+  moduleId: module.id,
+  providers: [BackendService]
+}) 
+export class LoginPageComponent implements OnInit {
+  
+  private drawer: DrawerClass;
+  private message: string;
 
-    private drawer: DrawerClass;
-    private username: string;
+  constructor(private page: Page, private backendService: BackendService, private settingsService: SettingsService, private routerExtensions: RouterExtensions) {
+    // Use the component constructor to inject providers.
+    this.drawer = globals.getDrawer();
+    page.actionBarHidden = true; 
+  }
+  
 
-
-    constructor(private page: Page, private backendService: BackendService, private settingsService: SettingsService, private routerExtensions: RouterExtensions) {
-        // Use the component constructor to inject providers.
-        this.drawer = globals.getDrawer();
-        page.actionBarHidden = true; 
-
-        let token = this.settingsService.getSetting(undefined, 61);
-        if (token == undefined) {
-            console.log("TokenSetting is undefined");
-            this.routerExtensions.navigate(["account", "login"], {
-                animated: true,
-                clearHistory: true,
-                transition: {
-                    name: "slideRight"
-                }
-            });
-        } else if (token.value == undefined){
-            console.log("Token is undefined");
-            this.routerExtensions.navigate(["account", "login"], {
-                animated: true,
-                clearHistory: true,
-                transition: {
-                    name: "slideRight"
-                }
-            });
-        } else {
-            console.log("Token is defined");
-            try {
-                this.backendService.getInfo()
-                .subscribe((result) => {
-                    console.dir(<any>result);
-                    if(<any>result.status == 202){
-                        this.userInfo = (<any>result).body.username
-                    } else {
-                        this.routerExtensions.navigate(["account", "login"], {
-                            animated: true,
-                            clearHistory: true,
-                            transition: {
-                                name: "slideRight"
-                            }
-                        });
-                    }
-                });
-            } catch (error) {
-                console.log("Error while getting username: " + error);
-                this.routerExtensions.navigate(["account", "login"], {
-                    animated: true,
-                    clearHistory: true,
-                    transition: {
-                        name: "slideRight"
-                    }
-                });
-            }
-            
+  login(loginName, password){
+    console.log("email: " + loginName + " passord: " + password);
+    this.backendService.login(loginName, password)
+    .subscribe((result) => {
+      let tokenSetting = this.settingsService.getSetting(undefined, 61);
+      if (tokenSetting == undefined){
+        tokenSetting = {
+          id: 61,
+          name: "tokenSetting",
+          type: "token",
+          value: undefined
         }
-    }
+      }
+      console.dir(result);
+      tokenSetting.value = (<any>result).body.access_token;
+      this.message = (<any>result).body.message;
+      this.settingsService.setSetting(tokenSetting);
+      this.routerExtensions.navigate(["account"], {
+        animated: true,
+        clearHistory: true,
+        transition: {
+          name: "slideLeft"
+        }
+      })
+    });
+  }
 
-    private userInfo;
 
-    logOut(){
-        let tokenSetting = this.settingsService.getSetting(undefined, 61);
-        this.backendService.logOut()
-        .subscribe((result) => {
-            console.dir(result);
-            if (<any>result.status == 200 || <any>result.status == 401){
-                if (tokenSetting == undefined){
-                    tokenSetting = {
-                    id: 61,
-                    name: "tokenSetting",
-                    type: "token",
-                    value: undefined
-                    }
-                    tokenSetting.value=undefined;
-                    this.settingsService.setSetting(tokenSetting);
-                }
-                this.routerExtensions.navigate(["account", "login"], {
-                    animated: true,
-                    clearHistory: true,
-                    transition: {
-                        name: "slideRight"
-                    }
-                });
-            } else {
 
-            }
-        });
-    }
+
+  ngOnInit() {
+
+  }
 
 }
