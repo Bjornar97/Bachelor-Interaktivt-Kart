149aaa135e5df29702b95993b9a287100542a344
Bjornar97 <hetlest@hotmail.com>
2019-05-10 14:36:44 +0200

    Lagde siden for lagrede turer

--- KartApp/src/app/account-page/saved-trips/saved-trips.component.ts
+++ KartApp/src/app/account-page/saved-trips/saved-trips.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { TripService } from '~/app/home-page/trip.service';
 import { Trip } from '~/app/tracker';
+import { Page, isAndroid } from 'tns-core-modules/ui/page/page';
+import { RouterExtensions } from 'nativescript-angular/router';
+import * as application from 'tns-core-modules/application';
+import { DrawerClass } from '~/app/drawer';
+import * as globals from '~/app/globals';
 
 @Component({
   selector: 'ns-saved-trips',
@@ -13,12 +18,39 @@ export class SavedTripsComponent implements OnInit {
 
   private savedTrips: Array<Trip>;
 
-  constructor(private tripService: TripService) { 
+  private drawer: DrawerClass;
 
+
+  constructor(page: Page, private tripService: TripService, private routerExtensions: RouterExtensions) { 
+    page.actionBarHidden = true;
+    this.drawer = globals.getDrawer();
   }
 
-  ngOnInit() {
+  private goBack() {
+    this.routerExtensions.navigate(["account"], {
+      animated: true,
+      clearHistory: true,
+      transition: {
+        name: "slideRight"
+      }
+    });
+  }
+
+  convertToJson(trip: Trip) {
+    return JSON.stringify(trip);
+  }
+
+  getTrips() {
     this.savedTrips = this.tripService.getBookmarkedTrips();
   }
 
+  ngOnInit() {
+    if (isAndroid){
+      application.android.on(application.AndroidApplication.activityBackPressedEvent, (args: any) => {
+        args.cancel = true;
+        this.goBack();
+      });
+    }
+    this.getTrips();
+  }
 }
