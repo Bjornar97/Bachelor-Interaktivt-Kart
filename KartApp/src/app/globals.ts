8017f125df1547cfabc69c7e833ab3bf4d52aaf5
mnkvm <35263582+mnkvm@users.noreply.github.com>
2019-05-14 22:20:18 +0200

    Endret array til dict

--- KartApp/src/app/globals.ts
+++ KartApp/src/app/globals.ts
@@ -143,17 +143,18 @@ export function showError(errorString: string) {
   }
 
   var CheckboxList: boolean[] = [];
+  export var CheckboxDict = {};
   
   export function getCheckboxList(id: number){
-    if (CheckboxList[id] == undefined){
+    if (CheckboxDict[id] == undefined){
       console.log("Creating CheckboxList entry");
-      CheckboxList[id] = false;
+      CheckboxDict[id] = false;
     }
-    return CheckboxList[id];
+    return CheckboxDict[id];
   }
 
   export function setCheckboxList(id: number, value: boolean){
-    CheckboxList[id] = value;
+    CheckboxDict[id] = value;
   }
 
   // Current friends-trip
